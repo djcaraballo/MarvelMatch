83ca671a83a686c5398cd28db175e73f78b82137
Dina <dcaraballo26@gmail.com>
2018-11-01 15:59:13 -0600

    Add tests for API calls and format API url

---
+++ src/Utils/API/API.tests.js
@@ -0,0 +1,28 @@
+import * as API from './'
+
+const mockData = {someData}
+describe('API', () => {
+  it('should call fetch with the correct params' () => {
+    const mockUrl = 'someUrl.com'
+    window.fetch = jest.fn().mockImplementation(() => {
+      Promise.resolve({
+        status: 200,
+        json: () => Promise.resolve(mockData)
+      })
+    })
+    API.fetchInfo(mockUrl)
+    expect(window.fetch).toHaveBeenCalledWith(mockUrl)
+  })
+
+  it('should throw an error if fetch call fails', () => {
+    const mockUrl = 'someUrl.com'
+    const expected = Error('Too bad your fetch failed, chump!')
+    window.fetch = jest.fn().mockImplementation(() => {
+      Promise.resolve({
+        status: 500,
+        json: () => Promise.resolve()
+      })
+    })
+    expect(API.fetchInfo(mockUrl)).rejects.toEqual(expected)
+  })
+})
--- src/Utils/API/index.js
+++ src/Utils/API/index.js
@@ -1,7 +1,7 @@
-// Public key
-// a4d95911b60999871aaed8a8c43823f6
+import { url } from 'API-key';
 
-// Private key
-//f1b690b8e89d7f112c6f33f956693b2f91d93400
-
-//ts = Date.now()
+export fetchInfo = () => {
+  const response = fetch(url)
+  const result = response.json()
+  return result
+}
