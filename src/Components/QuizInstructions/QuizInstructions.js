53ea5d227ce1b8abec07512223c7586edd27c3c8
Dina <dcaraballo26@gmail.com>
2018-11-02 17:41:25 -0600

    Add new directories

---
+++ src/Components/QuizInstructions/QuizInstructions.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const QuizInstructions = () => {
+  return (
+    <div>
+      <h1>Take the quiz to find your <span>Spirit Superhero</span>!</h1>
+      <p>We all know that no one in real life looks like a Marvel Comics superhero, but we all feel like one inside. Answer the questions in our brief quiz to choose your Spirit Superhero and avatar!</p>
+ 
+      <p>P.S. Be honest with yourself! Your matches will be sent to you based on your superhero and there's no point in dating a Black Widow or Wolverine if you could date a Wasp or Luke Cage!</p>
+    </div>
+  )
+}
+
+export default QuizInstructions
