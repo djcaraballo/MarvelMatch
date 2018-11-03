5db387945445c77effc21290531f86e3df619c06
Dina <dcaraballo26@gmail.com>
2018-11-03 11:40:56 -0600

    Add styling to LandingPage component

--- src/Components/LandingPage/LandingPage.js
+++ src/Components/LandingPage/LandingPage.js
@@ -1,16 +1,27 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 import './LandingPage.css'
 
 const LandingPage = () => {
   return (
-    <div className="logo-container">
-      <h1 className="marvel">MARVEL</h1>
-      <h2 className="match">MATCH</h2>
+    <div className="wrapper">
+      <div className="logo-container">
+        <h1 className="marvel">MARVEL</h1>
+        <h2 className="match">MATCH</h2>
+      </div>
       <img className="gif" src="../../assets/comic-flip.gif"/>
       <div className="links-container">
-        <h3>login</h3>
+        <NavLink 
+          to='../../Containers/UserLoginForm/UserLoginForm'
+          className='nav'>
+          <h3>login</h3>
+        </NavLink>
         <p><span className="line-span"></span>OR<span className="line-span"></span></p>
-        <h3>signup</h3>
+        <NavLink 
+          to='../../Components/NewHeroSignupForm/NewHeroSignupForm'
+          className='nav'>
+          <h3>signup</h3>
+        </NavLink>
       </div>
     </div>
   )
