e5c57ec1551e5542c478cc3aec16134c490e5a12
Dina <dcaraballo26@gmail.com>
2018-11-02 20:38:52 -0600

    Style LandingPage component

--- src/App/App.js
+++ src/App/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 import logo from '../logo.svg';
 import './App.css';
-// import { Cleaner } from '../Utils/Cleaners/'
 import * as Cleaner from '../Utils/Cleaners/'
 import NewHeroSignupForm from '../Components/NewHeroSignupForm/NewHeroSignupForm'
-import QuizInstructions from '../Components/QuizInstructions'
+import QuizInstructions from '../Components/QuizInstructions/QuizInstructions'
+import LandingPage from '../Components/LandingPage/LandingPage'
 
 class App extends Component {
 
@@ -17,20 +17,9 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <NewHeroSignupForm />
-          <QuizInstructions />
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          {/*<NewHeroSignupForm />*/}
+          {/*<QuizInstructions />*/}
+          <LandingPage />
         </header>
       </div>
     );
---
+++ src/Components/LandingPage/LandingPage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './LandingPage.css'
+
+const LandingPage = () => {
+  return (
+    <div className="logo-container">
+      <h1 className="marvel">MARVEL</h1>
+      <h2 className="match">MATCH</h2>
+      <img className="gif" src="../../assets/comic-flip.gif" />
+    </div>
+  )
+}
+
+export default LandingPage
