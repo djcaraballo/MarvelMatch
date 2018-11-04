4a8c0b33dccf4071c9e4eea987e3e6ba0c3e4ae6
Dina <dcaraballo26@gmail.com>
2018-11-04 09:11:56 -0700

    Add redux functionality

--- src/App/App.js
+++ src/App/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { addCharacters } from '../Actions/characterActions';
 import './App.css';
 import * as Cleaner from '../Utils/Cleaners/'
 import NewHeroSignupForm from '../Components/NewHeroSignupForm/NewHeroSignupForm'
@@ -7,7 +9,7 @@ import QuizInstructions from '../Components/QuizInstructions/QuizInstructions'
 import LandingPage from '../Components/LandingPage/LandingPage'
 import ErrorPage from '../Components/ErrorPage/ErrorPage'
 
-class App extends Component {
+export class App extends Component {
 
   async componentDidMount() {
     const cleanData = await Cleaner.getCharacterData()
@@ -34,4 +36,8 @@ class App extends Component {
   }
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return { characters: state.charactersReducer }
+}
+
+export default connect(mapStateToProps, null)(App);
