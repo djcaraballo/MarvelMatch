16fa57d1df045e3cf27abab5a16b65cf2ed7de44
Dina <dcaraballo26@gmail.com>
2018-11-04 11:55:14 -0700

    Add passing tests for cleaner

--- src/Utils/Cleaners/Cleaners.test.js
+++ src/Utils/Cleaners/Cleaners.test.js
@@ -1,5 +1,4 @@
 import React from 'react'
-// import * as Cleaner from './'
 import Cleaner from './'
 import * as API from '../API/'
 import { uncleanCharacters } from '../MockData/mockUncleanCharacters'
@@ -36,15 +35,43 @@ describe('Cleaners', () => {
   })
 
   describe('cleanCharacterCollection function', () => {
-    it('should call getCharacterData', async () => {
-      const mockCleaner = new Cleaner()
+    const mockCleaner = new Cleaner()
+
+    it('should call getCharacterData', () => {
       mockCleaner.getCharacterData = jest.fn()
       mockCleaner.cleanCharacterCollection()
       expect(mockCleaner.getCharacterData).toHaveBeenCalled()
     })
 
-    xit('should return an array of cleaned character objects', async () => {
-
+    it('should return an array of cleaned character objects', async () => {
+      mockCleaner.getCharacterData = jest.fn(() => {
+        return uncleanCharacters
+      })
+      const mockCleanCharacters = await mockCleaner.cleanCharacterCollection()
+      const expected = [
+        {
+          "id": 1011334,
+          "name": "3-D Man",
+          "description": "",
+          "imageURL": "http://i.annihil.us/u/prod/marvel/i/mg/c/e0/535fecbbb9784/standard_fantastic.jpg",
+          "siteURL": "http://marvel.com/characters/74/3-d_man?utm_campaign=apiRef&utm_source=a4d95911b60999871aaed8a8c43823f6"
+        },
+        {
+          "id": 1017100,
+          "name": "A-Bomb (HAS)",
+          "description": "Rick Jones has been Hulk's best bud since day one, but now he's more than a friend...he's a teammate! Transformed by a Gamma energy explosion, A-Bomb's thick, armored skin is just as strong and powerful as it is blue. And when he curls into action, he uses it like a giant bowling ball of destruction! ",
+          "imageURL": "http://i.annihil.us/u/prod/marvel/i/mg/3/20/5232158de5b16/standard_fantastic.jpg",
+          "siteURL": "http://marvel.com/characters/76/a-bomb?utm_campaign=apiRef&utm_source=a4d95911b60999871aaed8a8c43823f6"
+        },
+        {
+          "name": "A.I.M.",
+          "id": 1009144,
+          "description": "AIM is a terrorist organization bent on destroying the world.",
+          "imageURL": "http://i.annihil.us/u/prod/marvel/i/mg/6/20/52602f21f29ec/standard_fantastic.jpg",
+          "siteURL": "http://marvel.com/characters/77/aim.?utm_campaign=apiRef&utm_source=a4d95911b60999871aaed8a8c43823f6"
+        }
+      ]
+      expect(mockCleanCharacters).toEqual(expected)
     })
   })
 })
--- src/Utils/Cleaners/index.js
+++ src/Utils/Cleaners/index.js
@@ -1,33 +1,5 @@
 import * as API from '../API/'
 
-// export const getCharacterData = async () => {
-//   const characterData = await API.fetchAllCharacters()
-//   const describedChars = await filterCharacters(characterData)
-//   return describedChars
-// }
-
-// export const filterCharacters = async (characterArray) => {
-//   const newCharArray = characterArray.filter(character => {
-//     return character.description !== ""
-//   })
-//   return newCharArray
-// }
-
-// export const cleanCharacterCollection = async () => {
-//   const uncleanCollection =  await getCharacterData()
-//   const cleanCollection = uncleanCollection.map(character => {
-//     return ({
-//       name: character.name,
-//       id: character.id,
-//       description: character.description,
-//       imageURL: `${character.thumbnail.path}/standard_fantastic.${character.thumbnail.extension}`,
-//       siteURL: character.urls[0].url,
-//     })
-//   })
-//   console.log(cleanCollection)
-//   return cleanCollection
-// }
-
 class Cleaner {
   constructor() {
     this.fetchAllCharacters = API.fetchAllCharacters
@@ -57,7 +29,6 @@ class Cleaner {
         siteURL: character.urls[0].url,
       })
     })
-    console.log(cleanCollection)
     return cleanCollection
   }
 }
