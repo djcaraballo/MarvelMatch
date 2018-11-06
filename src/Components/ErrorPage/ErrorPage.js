4c3dfd7e74a508bbd9f79129c61de4025f3439c4
Dina <dcaraballo26@gmail.com>
2018-11-06 14:07:37 -0700

    Style ErrorPage component

--- src/Components/ErrorPage/ErrorPage.js
+++ src/Components/ErrorPage/ErrorPage.js
@@ -6,13 +6,13 @@ import HomePage from '../HomePage/HomePage'
 const ErrorPage = () => {
   return (
     <div className="error-wrapper">
-      <NavLink to='/Home' className='nav'>
-        <p className='home-link'>Back To Home</p>
-      </NavLink>
       <div className='warning-container'>
         <h1>404: I AM GROOT!</h1>
         <p className='translation'>Translation: Not quite sure where you were headed, friend, but this path leads to Knowhere and you'll never survive the trip!</p>
       </div>
+      <NavLink to='/quiz-land' className='nav'>
+        <p className='home-link'>Back to Safety!</p>
+      </NavLink>
     </div>
   )
 }
