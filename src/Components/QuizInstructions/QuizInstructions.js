249394eac115a9836c49c8805acc3c3160f03074
Dina <dcaraballo26@gmail.com>
2018-11-06 22:10:59 -0700

    Add styles to quiz instructions component

--- src/Components/QuizInstructions/QuizInstructions.js
+++ src/Components/QuizInstructions/QuizInstructions.js
@@ -1,10 +1,20 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
+import './QuizInstructions.css'
 
 const QuizInstructions = () => {
   return (
     <div>
-      <h1>Take the quiz to find your <span>Spirit Superhero</span>!</h1>
-      <p>We all know that no one in real life looks like a Marvel Comics superhero, but we all feel like one inside. Answer the questions in our brief quiz to choose your Spirit Superhero and avatar!</p>
+      <h1 className="directions-title">Take the quiz to find your <span>Spirit Superhero</span>!</h1>
+      <div className="directions-container">
+        <p className="directions">We all know that no one in real life looks like a Marvel Comics superhero, but we all feel like one inside. Answer the questions in our brief quiz to choose your Spirit Superhero and avatar!</p>
+
+      <NavLink
+        to='/quiz'
+        className='quiz-nav'>
+        <h3>Match Me!</h3>
+      </NavLink>
+      </div>
     </div>
   )
 }
