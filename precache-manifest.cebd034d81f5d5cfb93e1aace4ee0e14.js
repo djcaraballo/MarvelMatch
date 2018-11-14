ca39269a878426394db39acf4781ae20bf63cad8
Dina <dcaraballo26@gmail.com>
2018-11-14 11:59:07 -0700

    Updates

---
+++ precache-manifest.cebd034d81f5d5cfb93e1aace4ee0e14.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "cc09c099da1dfc87c4aa767777ea0767",
+    "url": "/MarvelMatch/home/static/media/groot-404.cc09c099.jpg"
+  },
+  {
+    "revision": "f898c1d68cd606b51fd6ae809634ae4e",
+    "url": "/MarvelMatch/home/static/media/comic-flip.f898c1d6.gif"
+  },
+  {
+    "revision": "346a9d96250190eaeeb50060e4e51a14",
+    "url": "/MarvelMatch/home/static/media/black-panther.346a9d96.jpg"
+  },
+  {
+    "revision": "2cfbf054d482010d5a0f",
+    "url": "/MarvelMatch/home/static/js/runtime~main.2cfbf054.js"
+  },
+  {
+    "revision": "aedb0595ee11b75d715d",
+    "url": "/MarvelMatch/home/static/js/main.aedb0595.chunk.js"
+  },
+  {
+    "revision": "4392118d25e6cdba19d2",
+    "url": "/MarvelMatch/home/static/js/1.4392118d.chunk.js"
+  },
+  {
+    "revision": "aedb0595ee11b75d715d",
+    "url": "/MarvelMatch/home/static/css/main.b3754aab.chunk.css"
+  },
+  {
+    "revision": "57323b56b07adc6abbabd841e619ee45",
+    "url": "/MarvelMatch/home/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/MarvelMatch/precache-manifest.ddb0a3970ac3e187c07f921845bab0e8.js"
+  "/MarvelMatch/home/precache-manifest.cebd034d81f5d5cfb93e1aace4ee0e14.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/MarvelMatch/index.html", {
+workbox.routing.registerNavigationRoute("/MarvelMatch/home/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
