3998bc2355abd45acca8e1840b1c2aafabfaeecb
Dina <dcaraballo26@gmail.com>
2018-11-14 11:55:46 -0700

    Updates

---
+++ precache-manifest.ddb0a3970ac3e187c07f921845bab0e8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "cc09c099da1dfc87c4aa767777ea0767",
+    "url": "/MarvelMatch/static/media/groot-404.cc09c099.jpg"
+  },
+  {
+    "revision": "f898c1d68cd606b51fd6ae809634ae4e",
+    "url": "/MarvelMatch/static/media/comic-flip.f898c1d6.gif"
+  },
+  {
+    "revision": "346a9d96250190eaeeb50060e4e51a14",
+    "url": "/MarvelMatch/static/media/black-panther.346a9d96.jpg"
+  },
+  {
+    "revision": "1ebb95583c385e690070",
+    "url": "/MarvelMatch/static/js/runtime~main.1ebb9558.js"
+  },
+  {
+    "revision": "c8681482fcea1fb51f43",
+    "url": "/MarvelMatch/static/js/main.c8681482.chunk.js"
+  },
+  {
+    "revision": "4392118d25e6cdba19d2",
+    "url": "/MarvelMatch/static/js/1.4392118d.chunk.js"
+  },
+  {
+    "revision": "c8681482fcea1fb51f43",
+    "url": "/MarvelMatch/static/css/main.a9cebe1e.chunk.css"
+  },
+  {
+    "revision": "0cd7a212a73903a32813a27f174779dd",
+    "url": "/MarvelMatch/index.html"
+  }
+];
